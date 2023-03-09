4fe21dc2cb0162ecc6eaeaaac522a00dbca08291
MarcosPotato <marquinhos2725@hotmail.com>
2023-03-09 10:29:47 -0300

    feat: creating import tasks from csv

--- src/utils/parse-csv.ts
+++ src/utils/parse-csv.ts
@@ -9,37 +9,15 @@ interface ParseCSVParams {
 const csvParse = parse({
   delimiter: ',',
   skipEmptyLines: true,
-  fromLine: 2 // skip the header line
+  fromLine: 2
 })
 
 export const parseCSV = async({ onReadFile, path }: ParseCSVParams) => {
-  /* const csvPath = new URL(path) */
-  
   const stream = fs.createReadStream(path)
   
   const linesParse = stream.pipe(csvParse)
   
   for await (const line of linesParse) {
     await onReadFile(line)
-    
-    /* const [title, description] = line
-
-    await fetch('http://localhost:3333/tasks', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title,
-        description,
-      })
-    }) */
-
-    // Uncomment this line to see the import working in slow motion (open the db.json)
-    // await wait(1000)
   }
 }
-
-/* function wait(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-} */
